011d9e3747784535da18be8abbf1adad81550551
situx <situx@users.noreply.github.com>
2024-01-23 02:29:38 +0000

    Deploying to gh-pages from @ archaeolink/SPP1630Harbours-RDF@07ce2abd896461589506b0f961f7959484f35f8a 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -103,21 +103,42 @@ var minivowlresult={
  ],
  "links": [
   {
-   "source": 0,
-   "target": 18,
-   "valueTo": "partOf",
+   "source": 7,
+   "target": 8,
+   "valueTo": "hasBeginning",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/partOf"
+   "uriTo": "http://www.w3.org/2006/time#hasBeginning"
   },
   {
    "source": 0,
-   "target": 17,
-   "valueTo": "partOf",
+   "target": 9,
+   "valueTo": "hasGeometry",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/partOf"
+   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
   },
   {
-   "source": 12,
+   "source": 17,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 11,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 2,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 1,
    "target": 10,
    "valueTo": "creator",
    "propertyTo": "class",
@@ -131,14 +152,7 @@ var minivowlresult={
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 17,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 11,
+   "source": 12,
    "target": 10,
    "valueTo": "creator",
    "propertyTo": "class",
@@ -159,13 +173,6 @@ var minivowlresult={
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 1,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
    "source": 18,
    "target": 10,
    "valueTo": "creator",
@@ -173,11 +180,11 @@ var minivowlresult={
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 2,
-   "target": 10,
-   "valueTo": "creator",
+   "source": 0,
+   "target": 7,
+   "valueTo": "hasTime",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+   "uriTo": "http://www.w3.org/2006/time#hasTime"
   },
   {
    "source": 0,
@@ -188,24 +195,31 @@ var minivowlresult={
   },
   {
    "source": 0,
-   "target": 9,
-   "valueTo": "hasGeometry",
+   "target": 17,
+   "valueTo": "partOf",
    "propertyTo": "class",
-   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
+   "uriTo": "http://purl.org/dc/terms/partOf"
   },
   {
    "source": 0,
-   "target": 12,
-   "valueTo": "isReferencedBy",
+   "target": 18,
+   "valueTo": "partOf",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+   "uriTo": "http://purl.org/dc/terms/partOf"
   },
   {
-   "source": 0,
-   "target": 4,
-   "valueTo": "isReferencedBy",
+   "source": 2,
+   "target": 13,
+   "valueTo": "issuer",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+   "uriTo": "http://purl.org/ontology/bibo/issuer"
+  },
+  {
+   "source": 7,
+   "target": 8,
+   "valueTo": "hasEnd",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2006/time#hasEnd"
   },
   {
    "source": 0,
@@ -216,7 +230,14 @@ var minivowlresult={
   },
   {
    "source": 0,
-   "target": 3,
+   "target": 12,
+   "valueTo": "isReferencedBy",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+  },
+  {
+   "source": 0,
+   "target": 2,
    "valueTo": "isReferencedBy",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/terms/isReferencedBy"
@@ -230,41 +251,34 @@ var minivowlresult={
   },
   {
    "source": 0,
-   "target": 2,
+   "target": 4,
    "valueTo": "isReferencedBy",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/terms/isReferencedBy"
   },
   {
-   "source": 7,
-   "target": 8,
-   "valueTo": "hasEnd",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2006/time#hasEnd"
-  },
-  {
-   "source": 7,
-   "target": 8,
-   "valueTo": "hasBeginning",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2006/time#hasBeginning"
-  },
-  {
    "source": 0,
-   "target": 7,
-   "valueTo": "hasTime",
+   "target": 3,
+   "valueTo": "isReferencedBy",
    "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2006/time#hasTime"
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
   },
   {
-   "source": 2,
-   "target": 13,
-   "valueTo": "issuer",
+   "source": 11,
+   "target": 14,
+   "valueTo": "publisher",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/ontology/bibo/issuer"
+   "uriTo": "http://purl.org/dc/terms/publisher"
   },
   {
    "source": 12,
+   "target": 14,
+   "valueTo": "publisher",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/publisher"
+  },
+  {
+   "source": 2,
    "target": 14,
    "valueTo": "publisher",
    "propertyTo": "class",
@@ -278,21 +292,7 @@ var minivowlresult={
    "uriTo": "http://purl.org/dc/terms/publisher"
   },
   {
-   "source": 11,
-   "target": 14,
-   "valueTo": "publisher",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/publisher"
-  },
-  {
    "source": 1,
-   "target": 14,
-   "valueTo": "publisher",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/publisher"
-  },
-  {
-   "source": 2,
    "target": 14,
    "valueTo": "publisher",
    "propertyTo": "class",
