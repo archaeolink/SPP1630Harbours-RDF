ee32c63efbe601a1d79ca30d2ee36da4ab38046d
situx <situx@users.noreply.github.com>
2024-01-27 23:12:43 +0000

    Deploying to gh-pages from @ archaeolink/SPP1630Harbours-RDF@6df32a20663c4a8fa9785b7186cf4cf8a745889a 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -103,13 +103,6 @@ var minivowlresult={
  ],
  "links": [
   {
-   "source": 7,
-   "target": 8,
-   "valueTo": "hasBeginning",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2006/time#hasBeginning"
-  },
-  {
    "source": 0,
    "target": 16,
    "valueTo": "isOutputOf",
@@ -117,92 +110,8 @@ var minivowlresult={
    "uriTo": "http://purl.org/cerif/frapo/isOutputOf"
   },
   {
-   "source": 7,
-   "target": 8,
-   "valueTo": "hasEnd",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2006/time#hasEnd"
-  },
-  {
-   "source": 2,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 1,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 11,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 12,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
    "source": 0,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 18,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 17,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 3,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 4,
-   "target": 10,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 0,
-   "target": 17,
-   "valueTo": "partOf",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/partOf"
-  },
-  {
-   "source": 0,
-   "target": 18,
-   "valueTo": "partOf",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/partOf"
-  },
-  {
-   "source": 0,
-   "target": 2,
+   "target": 4,
    "valueTo": "isReferencedBy",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/terms/isReferencedBy"
@@ -210,6 +119,13 @@ var minivowlresult={
   {
    "source": 0,
    "target": 1,
+   "valueTo": "isReferencedBy",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+  },
+  {
+   "source": 0,
+   "target": 2,
    "valueTo": "isReferencedBy",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/terms/isReferencedBy"
@@ -237,10 +153,24 @@ var minivowlresult={
   },
   {
    "source": 0,
-   "target": 4,
-   "valueTo": "isReferencedBy",
+   "target": 17,
+   "valueTo": "partOf",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+   "uriTo": "http://purl.org/dc/terms/partOf"
+  },
+  {
+   "source": 0,
+   "target": 18,
+   "valueTo": "partOf",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/partOf"
+  },
+  {
+   "source": 7,
+   "target": 8,
+   "valueTo": "hasBeginning",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2006/time#hasBeginning"
   },
   {
    "source": 0,
@@ -257,7 +187,84 @@ var minivowlresult={
    "uriTo": "http://www.w3.org/2006/time#hasTime"
   },
   {
+   "source": 18,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 4,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 1,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 0,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
    "source": 2,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 11,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 12,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 17,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 3,
+   "target": 10,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 7,
+   "target": 8,
+   "valueTo": "hasEnd",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2006/time#hasEnd"
+  },
+  {
+   "source": 2,
+   "target": 13,
+   "valueTo": "issuer",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/ontology/bibo/issuer"
+  },
+  {
+   "source": 4,
    "target": 14,
    "valueTo": "publisher",
    "propertyTo": "class",
@@ -265,6 +272,13 @@ var minivowlresult={
   },
   {
    "source": 1,
+   "target": 14,
+   "valueTo": "publisher",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/publisher"
+  },
+  {
+   "source": 2,
    "target": 14,
    "valueTo": "publisher",
    "propertyTo": "class",
@@ -283,20 +297,6 @@ var minivowlresult={
    "valueTo": "publisher",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/terms/publisher"
-  },
-  {
-   "source": 4,
-   "target": 14,
-   "valueTo": "publisher",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/terms/publisher"
-  },
-  {
-   "source": 2,
-   "target": 13,
-   "valueTo": "issuer",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/ontology/bibo/issuer"
   }
  ]
 }
